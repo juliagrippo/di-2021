ba40e1962ad0ad38a4b1f31b906e7475c37864a6
juliagrippo <78823753+juliagrippo@users.noreply.github.com>
2021-03-02 13:06:34 -0500

    Updated Code Sketches

--- 2.mapping-time/date-3/sketch.js
+++ 2.mapping-time/date-3/sketch.js
@@ -1,13 +1,7 @@
-function pointAt(cx, cy, angle, dist){
-  var theta = angle/360 * TWO_PI
-  return {x:cx+cos(theta) * dist,
-          y:cy+sin(theta) * dist}
-}
 
 function setup() {
   // set the width & height of the sketch
-  createCanvas(500, 500);
-  rectMode(CENTER)
+  createCanvas(500, 500)
 
   // print the time to the console once at the beginning of the run. try opening up the
   // web inspector and poking around to see the various values the clock function gives you
@@ -18,16 +12,6 @@ function setup() {
 function draw() {
   // check the clock for the current time and unpack some of its fields to generate a time-string
   var now = clock()
-
-  var minAngle = map(now.sec, 0, 60, 0, 360)
-
-  var secAngle = map(now.progress.sec, 0, 1, 0, 360
-
-  var origin = {x:width/2, y:height/2}
-
-  var minLocation = pointAt(origin.x, origin.y, minAngle, 80)
-
-  var secLocation = pointAt(origin.x, origin.y, secAngle, 50)
 
   // set the background to 'white' – you can also specify colors use integers, hex-color strings and more.
   // note that setting the background also clears the canvas from our previous round of drawing
@@ -43,31 +27,23 @@ function draw() {
   text(now.text.date, 30, 50)
   text(now.text.time, 30, 100)
 
-  noFill();
-  stroke(255);
-  strokeWeight(3);
-  circle(250,275,375);
-
-  fill(62,62,255);
+  fill(147,94,0);
   noStroke(255);
-  strokeWeight(3);
-  circle(origin.x, origin.y, 35);
+  rect(100,130,140,140);
 
-  noFill();
-  stroke(255);
-  circle(250,275,260);
-
-  fill(64,151,255);
+  fill(147,94,0);
   noStroke(255);
-  strokeWeight(3);
-  circle(250,147,25);
+  rect(100,280,140,140);
 
-  noFill();
-  stroke(255);
-  circle(250,275,150);
-
-  fill(67,220,255);
+  fill(147,94,0);
   noStroke(255);
-  strokeWeight(3);
-  circle(250,202,20);
+  rect(250,130,140,140);
+
+  fill(147,94,0);
+  noStroke(255);
+  rect(250,280,140,140);
+
+  fill(0);
+  noStroke(255);
+  circle(140,170,60);
 }
